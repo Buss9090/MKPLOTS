e7a8f2daac91cca05d44c5a2826f2a0bb126a42e
Buss9090 <vanessadbuss@gmail.com>
2025-04-22 21:37:11 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from rhvendas import RHvendas
+from rhvendas import rhvendas
 
 __CHANNEL_USERNAME__ = "mk_plots"
 __GROUP_USERNAME__   = "(21)998189601"
